e6752362ee781f9a468573ab5c437de656004049
ds960066-pixel <ds960066@gmail.com>
2025-12-14 23:02:23 +0530

    Create Institute.js
    add institute model

---
+++ models/Institute.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const instituteSchema = new mongoose.Schema(
+  {
+    uid: {
+      type: String,
+      required: true,
+      unique: true, // Firebase UID
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    subjectsNeeded: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Institute", instituteSchema);
