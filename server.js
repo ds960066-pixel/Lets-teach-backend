e1210549df134cf3871ca5ce09b6ea7a3cf495d1
ds960066-pixel <ds960066@gmail.com>
2025-12-14 13:41:48 +0530

    Update server.js
    fix server listen log

--- server.js
+++ server.js
@@ -17,5 +17,6 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
+
 });
