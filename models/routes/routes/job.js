565ba4192e7a97f78b33226812e6d70d6b9bc533
ds960066-pixel <ds960066@gmail.com>
2026-01-03 16:57:15 +0530

    Delete models/routes/routes/job.js

--- models/routes/routes/job.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/browse", (req, res) => {
-  res.json({ success: true, jobs: [] });
-});
-
-module.exports = router;
